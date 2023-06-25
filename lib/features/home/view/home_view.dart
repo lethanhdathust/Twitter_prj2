97fda9a841bb829a2688cde7e4399846a381bf9c
CihK0ng <gunnymagi@gmail.com>
2023-06-26 01:02:29 +0700

    commit code

---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeView extends StatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const HomeView(),
+      );
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _page = 0;
+  final appBar = UIConstants.appBar();
+
+  void onPageChange(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+
+  onCreateTweet() {
+    Navigator.push(context, CreateTweetScreen.route());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _page == 0 ? appBar : null,
+      body: IndexedStack(
+        index: _page,
+        children: UIConstants.bottomTabBarPages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onCreateTweet,
+      ),
+      drawer: const SideDrawer(),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _page,
+        onTap: onPageChange,
+        backgroundColor: Pallete.backgroundColor,
+        items: [
+          BottomNavigationBarItem(),
+          BottomNavigationBarItem(),
+          BottomNavigationBarItem(),
+        ],
+      ),
+    );
+  }
+}
