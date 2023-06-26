17b1525ec0c41ee50acf2c7314683f0343947cca
NguyenLeAnh2002 <nguyenleanh2002@gmail.com>
2023-06-26 16:17:29 +0700

    apis update

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:twitter_clone/core/core.dart';
 import 'package:twitter_clone/core/providers.dart';
+
 // inject the dependencies form another provider
 final authAPIProvider = Provider((ref) {
   final account = ref.watch(appwriteAccountProvider);
@@ -15,22 +16,38 @@ final authAPIProvider = Provider((ref) {
 // want to access user related data ->model.account
 abstract class IAuthAPI {
   // void signup();
-
   FutureEither<model.Account> signUp(
       {required String email, required String password});
+
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  });
+  Future<model.Account?> currentUserAccount();
+  FutureEitherVoid logout();
 }
 
 class AuthAPI implements IAuthAPI {
   final Account _account;
-  // AuthAPI({
-  // required Account account,
-  // }) : _account = account;
+
   AuthAPI({
     required Account account,
   }) : _account = account;
 // Khi khởi tạo đối tượng AuthAPI,
   // Phần :_account=account có nghĩa là khi một đối tượng AuthAPI được khởi tạo,
   // thuộc tính _account sẽ được khởi tạo với giá trị của tham số account.
+
+  @override
+  Future<model.Account?> currentUserAccount() async {
+    try {
+      return await _account.get();
+    } on AppwriteException {
+      return null;
+    } catch (e) {
+      return null;
+    }
+  }
+
   @override
   FutureEither<model.Account> signUp(
       {required String email, required String password}) async {
@@ -43,6 +60,46 @@ class AuthAPI implements IAuthAPI {
           Failure(e.message ?? "some unexcepted error occured", stackTrace));
     } catch (e, stackTrace) {
       return left(Failure(e.toString(), stackTrace));
+    }
+  }
+
+  @override
+  FutureEither<model.Session> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final session = await _account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      return right(session);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+
+  @override
+  FutureEitherVoid logout() async {
+    try {
+      await _account.deleteSession(
+        sessionId: 'current',
+      );
+      return right(null);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occurred', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
     }
   }
 }
