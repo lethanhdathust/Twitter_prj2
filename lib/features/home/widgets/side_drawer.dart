ca0f6c5f3085243a7ca172abe46d63349abb12d2
CihK0ng <gunnymagi@gmail.com>
2023-06-28 10:11:14 +0700

    build

--- lib/features/home/widgets/side_drawer.dart
+++ lib/features/home/widgets/side_drawer.dart
@@ -8,11 +8,8 @@ class SideDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentUser = ref.watch(currentUserDetailsProvider).value;
+  
 
-    if (currentUser == null) {
-      return const Loader();
-    }
     return SafeArea(
         child: Drawer(
       backgroundColor: Pallete.backgroundColor,
