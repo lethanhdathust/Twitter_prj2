aad77ee0b35727d4a717ffda8c11f7098c2efd19
CihK0ng <gunnymagi@gmail.com>
2023-07-15 11:39:15 +0700

    commit lines

--- lib/features/explore/view/explore_view.dart
+++ lib/features/explore/view/explore_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/theme/pallete.dart';
 
 class ExploreVIew extends ConsumerStatefulWidget {
   const ExploreVIew({super.key});
@@ -9,16 +10,43 @@ class ExploreVIew extends ConsumerStatefulWidget {
 }
 
 class _ExploreVIewState extends ConsumerState<ExploreVIew> {
-  final SearchController = Text
+  final SearchController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    SearchController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final appBarTextFieldBorder = OutlineInputBorder(
+        borderRadius: BorderRadius.circular(50),
+        borderSide: const BorderSide(
+          color: Pallete.searchBarColor,
+        ));
+
     return Scaffold(
       appBar: AppBar(
-        title: SizeBox(height: 50,
-        child: TextField(
-        controller: ,
-        )
+// ở đây cta tạo vùng tìm kiếm cho chức năng tìm kiếm của appbar
+//
+
+        title: SizeBox(
+          height: 50,
+          child: TextField(
+            controller: SearchController,
+            decoration: InputDecoration(
+                 contentPadding: const EdgeInsets.all(10).copyWith(
+                  left: 20,
+                ),
+                fillColor: Pallete.searchBarColor,
+                filled: true,
+                enabledBorder: appBarTextFieldBorder,
+                focusedBorder: appBarTextFieldBorder,
+                hintText: "Search Twitter",
+          ),
+        ),
+      ),
       ),
     );
   }
